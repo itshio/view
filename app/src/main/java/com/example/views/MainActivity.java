2db3c190ebeddd271c1309f62d0629c6fd586a71
darkclif <morpheoosmatrix@gmail.com>
2018-01-26 13:26:31 +0100

    views pruebas boton

--- app/src/main/java/com/example/views/MainActivity.java
+++ app/src/main/java/com/example/views/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     TextView tvEtiqueta, resultado;
+    ImageView ivimagen;
 
-    T
+
 
 
 
@@ -26,18 +28,33 @@ public class MainActivity extends AppCompatActivity {
         resultado = (TextView)findViewById(R.id.resultado);
 
 
+        /*
+        ivimagen = (ImageView)findViewById((R.id.maivplus));
+
+        //evento click imagen
+        ivimagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultado.setText("Has pulsado el boton");}
+            }
+        });
+
+        */
+
+
         //editar el texto de un TextView
 
-        tvEtiqueta.setText("nino");
+        tvEtiqueta.setText("Hola mundo");
 
         //obtener valor de un TextView
 
-        String contenidoetiqueta = tvEtiqueta.getText().toString()
+        String contenidoetiqueta = tvEtiqueta.getText().toString();
 
                 resultado.setText(contenidoetiqueta);
 
 
 
+        //ImageView
 
 
 
@@ -45,9 +62,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    }//fin oncreate
 
 
+
+
+
+    } //fin oncreate
+
+
+    //para usar una imagen como boton
+
+
+    public void escribirTextView(View view){resultado.setText("Has pulsado el boton");
+
+    }
 
 
 
